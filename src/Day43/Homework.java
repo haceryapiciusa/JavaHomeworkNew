4f53fe0df30e894df4ae8ce494efd4803abfca56
haceryapiciusa <56479407+haceryapiciusa@users.noreply.github.com>
2019-12-03 13:39:36 -0500

    newww

---
+++ src/Day43/Homework.java
@@ -0,0 +1,11 @@
+package Day43;
+
+public class Homework {
+    // Let's write a Online-Shop that sells books: ChildrenBook & Comics
+    // use inheritance, polymorphism and encapsulation while doing this
+
+    // Note: this HW is open-ended, so there a lot of way to solve it
+    // task about salary is similar to this
+
+    // Hint: you can use Book as parent class and define price() method there
+}
